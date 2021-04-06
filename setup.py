308f6cd9c351d866fceaa6d8525799f8ff59ca5e
Charles F <charlesf.0@gmail.com>
2021-04-06 15:50:28 +0900

    Update setup.py
    setup for dev and test

--- setup.py
+++ setup.py
@@ -6,11 +6,25 @@
     PyScaffold helps you to put up the scaffold of your new Python project.
     Learn more under: https://pyscaffold.org/
 """
-from setuptools import setup
+from setuptools import setup, find_packages
+
+base_packages = []
+
+test_packages = ["pytest>=5.4.3", "black>=19.10b0", "flake8>=3.8.3"]
+
+util_packages = ["pre-commit>=2.6.0"]
+
+dev_packages = test_packages + util_packages
+
 
 if __name__ == "__main__":
     try:
-        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+        setup(
+            name="packagename",
+            version="0.0.1",
+            packages=find_packages(),
+            extras_require={"dev": dev_packages, "test": test_packages},
+        )
     except:  # noqa
         print(
             "\n\nAn error occurred while building the project, "
